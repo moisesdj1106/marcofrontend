29c78de766ef48774b37d6e858df39699cc7187c
moisesdj1104 <moises.becerra07@gmail.com>
2026-06-04 17:03:20 -0400

    ves

--- src/views/admin/Dashboard.jsx
+++ src/views/admin/Dashboard.jsx
@@ -47,9 +47,10 @@ const Dashboard = () => {
 
   const formatCOP = (val) => {
     return new Intl.NumberFormat('es-CO', {
-      style: 'currency',
-      currency: 'COP',
-      minimumFractionDigits: 0
+    style: 'currency',
+    currency: 'VES',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
     }).format(val);
   };
 
